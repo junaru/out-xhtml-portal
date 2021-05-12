c294d739ebaa0a03d2feb092117b0a490ab7ceb9
Justinas Naruševičius <contact@junaru.com>
2021-05-12 21:34:16 +0300

    use layout sin weather views

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>weather</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', 'MeteoLT')
+
+@section('body')
 <form action="{{route('weather.redirect')}}" method="POST">
 @csrf
-<select class="form-control" id="place" name="place" required focus>
+<select name="place" required focus>
 <option value="" disabled selected>Please select a place</option>        
 @foreach($places as $name => $value)
 <option value="{{$value}}">{{ $name }}</option>
@@ -15,5 +13,4 @@
 </select>
 <input type="submit" value="Go">
 </form>
-</body>
-</html>
+@endsection
--- resources/views/weather/place.blade.php
+++ resources/views/weather/place.blade.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>{{$place}}</title>
-<style>
-div { margin: 0px; }
-div.nd { width: 100%;background-color:#45668a;color:white;text-align:center;}
-div.e { background-color:#c9d7e7 }
-div.o { background-color:#e7eaee }
-body {margin: 0px; }
-</style>
-</head>
-<body style="margin: 0px;">
-<div class="nd">Generated at: {{$forecast_creation_date}}</div>
+@extends('layouts.app')
+
+@section('title', $place)
+
+@section('css')
+div, body { margin: 0px; }
+@endsection
+
+@section('body')
+<div>Generated at: {{$forecast_creation_date}}</div>
 @foreach($forecasts as $forecast)
 @if($forecast['hour'] == "00:00")
-<div class="nd">##########<br>{{$forecast['date']}}<br>##########</div>
+<hr><b>{{$forecast['date']}}</b><hr>
 @endif
-<div class="{{$loop->even ? 'e' : 'o'}}">{{ $forecast['hour'] }} {{ $forecast['temp'] }}°c {!! $forecast['rain'] > 0 ? '<b>': '' !!}{{ $forecast['rain'] }}mm{!! $forecast['rain'] > 0 ? '</b>' : '' !!}<br>{{ $forecast['wind']}}-{{$forecast['gust']}}m/s {{ $forecast['wdir']}}°</div>
-@endforeach 
-</body>
-</html>
+<div>{{ $forecast['hour'] }} {{ $forecast['temp'] }}°c {!! $forecast['rain'] > 0 ? '<b>': '' !!}{{ $forecast['rain'] }}mm{!! $forecast['rain'] > 0 ? '</b>' : '' !!}<br>{{ $forecast['wind']}}-{{$forecast['gust']}}m/s {{ $forecast['wdir']}}°</div>
+@endforeach
+@endsection
